2a8baaf901693bb9491aadba54d441d9282ef9f4
Natalia Graciano <nataliavgraciano@gmail.com>
2023-10-11 22:06:36 -0300

    Add polimorfismo example

--- src/Autodromo.java
+++ src/Autodromo.java
@@ -2,10 +2,16 @@ public class Autodromo {
     public static void main(String[] args) {
         Carro jeep = new Carro();
         jeep.setChassi("89278");
-        jeep.ligar();
+        //jeep.ligar();
 
         Moto z400 = new Moto();
         z400.setChassi("489200");
-        z400.ligar();
+        //z400.ligar();
+        
+        // polimorfismo
+        Veiculo coringa = z400;
+    
+        coringa.ligar();
     }
+
 }
